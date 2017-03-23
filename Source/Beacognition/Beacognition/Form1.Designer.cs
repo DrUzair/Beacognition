75992bfdbd77775780e7290db0f84c5782200dff
Dr Uzair Ahmad <DrUzair@users.noreply.github.com>
2017-03-23 09:30:37 -0400

    Add files via upload

---
+++ Source/Beacognition/Beacognition/Form1.Designer.cs
@@ -0,0 +1,251 @@
+namespace Beacognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Science Building");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Floor 1");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Computational Physics Lab");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Photo Electronics Lab");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Institute of Natural Sciences");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Natural Sciences Lecture Rooms");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Floor 2");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Robotics Labs");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Bio Medical Lecture Rooms");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Bio Medical Engg Labs");
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Admin Offices");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Floor 3");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Computer Engg Labs");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Radio Engg Labs");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Impedance Imaging Labs");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Faculty Offices");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Floor 4");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Student Union Offices");
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Radio Engineering Institute");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Bio Medical Labs");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Lecture Rooms");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Floor 5");
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Laser Engineering Labs");
+            System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Communications Labs");
+            System.Windows.Forms.TreeNode treeNode25 = new System.Windows.Forms.TreeNode("Astrophysics Labs");
+            System.Windows.Forms.TreeNode treeNode26 = new System.Windows.Forms.TreeNode("Ultrasonic Engg Labs");
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.menuItem4 = new System.Windows.Forms.MenuItem();
+            this.menuItem5 = new System.Windows.Forms.MenuItem();
+            this.menuItem6 = new System.Windows.Forms.MenuItem();
+            this.menuItem7 = new System.Windows.Forms.MenuItem();
+            this.menuItem8 = new System.Windows.Forms.MenuItem();
+            this.menuItem9 = new System.Windows.Forms.MenuItem();
+            this.menuItem10 = new System.Windows.Forms.MenuItem();
+            this.menuItem11 = new System.Windows.Forms.MenuItem();
+            this.menuItem12 = new System.Windows.Forms.MenuItem();
+            this.menuItem13 = new System.Windows.Forms.MenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            this.mainMenu1.MenuItems.Add(this.menuItem5);
+            this.mainMenu1.MenuItems.Add(this.menuItem8);
+            this.mainMenu1.MenuItems.Add(this.menuItem10);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(13, 32);
+            this.treeView1.Name = "treeView1";
+            treeNode3.Text = "Computational Physics Lab";
+            treeNode4.Text = "Photo Electronics Lab";
+            treeNode5.Text = "Institute of Natural Sciences";
+            treeNode6.Text = "Natural Sciences Lecture Rooms";
+            treeNode2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            treeNode2.Text = "Floor 1";
+            treeNode8.Text = "Robotics Labs";
+            treeNode9.Text = "Bio Medical Lecture Rooms";
+            treeNode10.Text = "Bio Medical Engg Labs";
+            treeNode11.Text = "Admin Offices";
+            treeNode7.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode9,
+            treeNode10,
+            treeNode11});
+            treeNode7.Text = "Floor 2";
+            treeNode13.Text = "Computer Engg Labs";
+            treeNode14.Text = "Radio Engg Labs";
+            treeNode15.Text = "Impedance Imaging Labs";
+            treeNode16.Text = "Faculty Offices";
+            treeNode12.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16});
+            treeNode12.Text = "Floor 3";
+            treeNode18.Text = "Student Union Offices";
+            treeNode19.Text = "Radio Engineering Institute";
+            treeNode20.Text = "Bio Medical Labs";
+            treeNode21.Text = "Lecture Rooms";
+            treeNode17.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode18,
+            treeNode19,
+            treeNode20,
+            treeNode21});
+            treeNode17.Text = "Floor 4";
+            treeNode23.Text = "Laser Engineering Labs";
+            treeNode24.Text = "Communications Labs";
+            treeNode25.Text = "Astrophysics Labs";
+            treeNode26.Text = "Ultrasonic Engg Labs";
+            treeNode22.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode23,
+            treeNode24,
+            treeNode25,
+            treeNode26});
+            treeNode22.Text = "Floor 5";
+            treeNode1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2,
+            treeNode7,
+            treeNode12,
+            treeNode17,
+            treeNode22});
+            treeNode1.Text = "Science Building";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            this.treeView1.Size = new System.Drawing.Size(224, 224);
+            this.treeView1.TabIndex = 0;
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.MenuItems.Add(this.menuItem2);
+            this.menuItem1.MenuItems.Add(this.menuItem3);
+            this.menuItem1.MenuItems.Add(this.menuItem4);
+            this.menuItem1.Text = "SMA";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "Add SMA";
+            // 
+            // menuItem3
+            // 
+            this.menuItem3.Text = "Delete SMA";
+            // 
+            // menuItem4
+            // 
+            this.menuItem4.Text = "Modify SMA";
+            // 
+            // menuItem5
+            // 
+            this.menuItem5.MenuItems.Add(this.menuItem6);
+            this.menuItem5.MenuItems.Add(this.menuItem7);
+            this.menuItem5.Text = "Training";
+            // 
+            // menuItem6
+            // 
+            this.menuItem6.Text = "Start";
+            // 
+            // menuItem7
+            // 
+            this.menuItem7.Text = "Stop";
+            // 
+            // menuItem8
+            // 
+            this.menuItem8.MenuItems.Add(this.menuItem9);
+            this.menuItem8.MenuItems.Add(this.menuItem13);
+            this.menuItem8.Text = "Localization";
+            // 
+            // menuItem9
+            // 
+            this.menuItem9.Text = "Start";
+            // 
+            // menuItem10
+            // 
+            this.menuItem10.MenuItems.Add(this.menuItem11);
+            this.menuItem10.MenuItems.Add(this.menuItem12);
+            this.menuItem10.Text = "Exit";
+            // 
+            // menuItem11
+            // 
+            this.menuItem11.Text = "Save Changes";
+            // 
+            // menuItem12
+            // 
+            this.menuItem12.Text = "Discard Changes";
+            // 
+            // menuItem13
+            // 
+            this.menuItem13.Text = "Stop";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 20);
+            this.label1.Text = "Semantically Meaningful Areas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "Beacognition System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.MenuItem menuItem4;
+        private System.Windows.Forms.MenuItem menuItem5;
+        private System.Windows.Forms.MenuItem menuItem6;
+        private System.Windows.Forms.MenuItem menuItem7;
+        private System.Windows.Forms.MenuItem menuItem8;
+        private System.Windows.Forms.MenuItem menuItem9;
+        private System.Windows.Forms.MenuItem menuItem10;
+        private System.Windows.Forms.MenuItem menuItem11;
+        private System.Windows.Forms.MenuItem menuItem12;
+        private System.Windows.Forms.MenuItem menuItem13;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
